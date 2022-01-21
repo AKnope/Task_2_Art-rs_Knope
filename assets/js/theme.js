b870382c3d0e076cc57f8481e20674f068115b70
AKnope <artursknope2@gmail.com>
2022-01-21 17:30:39 +0200

    update on footer and photo collage

---
+++ assets/js/theme.js
@@ -0,0 +1,13 @@
+function toggle() {
+    //get element id of the block that is going to be toggled
+    var toggledisplay = document.getElementById('toggle-display');
+    //define style
+    var display = toggledisplay.style.display;
+    // if display:block change to display:none, vice versa
+    if (display == 'block') {
+      toggledisplay.style.display = 'none';
+    }
+    else {
+      toggledisplay.style.display = 'block';
+    }
+}

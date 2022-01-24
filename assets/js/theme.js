a5aa6739bdde8241b9d67dd39c9046f369c1ca7b
AKnope <artursknope2@gmail.com>
2022-01-24 16:17:17 +0200

    completed footer, yellow footer in main page, image collage and navbar fix

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,8 +1,12 @@
 function toggle() {
     //get element id of the block that is going to be toggled
     var toggledisplay = document.getElementById('toggle-display');
+
+
     //define style
     var display = toggledisplay.style.display;
+
+
     // if display:block change to display:none, vice versa
     if (display == 'block') {
       toggledisplay.style.display = 'none';
